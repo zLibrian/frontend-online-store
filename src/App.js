dc4dae6b1cbead329e333e0c4c39dfd3caec4347
Alex <alexcorreas1994@gmail.com>
2021-09-21 18:06:44 -0300

    APIs

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RecipeAppProvider from './context/RecipesAppProvider';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <RecipeAppProvider>
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+      </RecipeAppProvider>
+
     </div>
   );
 }
+
+// const test = async () => {
+//   console.log(await fetchAPI(ingredientsFoodAPI));
+// };
+
+// const test2 = async () => {
+//   console.log(await fetchAPI(areasFoodAPI));
+// };
+
+// const test3 = async () => {
+//   console.log(await fetchAPI(categoryFoodAPI));
+// };
+
+// test();
+// test2();
+// test3();
 
 export default App;
