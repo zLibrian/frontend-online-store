89d0c91e6ccfc784896b9c331d13a2e1637f5e48
andre9653 <andresena9653@gmail.com>
2021-09-23 15:46:24 -0300

    Requisito 13

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,14 +1,47 @@
 import React, { useState } from 'react';
 import searchIcon from '../images/searchIcon.svg';
 
-export default function SearchInput() {
-  const [toogleInput, setToogleInput] = useState(false);
+function renderSearchBar() {
   return (
     <div>
-      <button type="button" onClick={ () => setToogleInput(!toogleInput) }>
+      <input type="text" data-testid="search-input" />
+      <label htmlFor="ingredient">
+        Ingredientes
+        <input
+          id="ingredient"
+          type="radio"
+          data-testid="ingredient-search-radio"
+        />
+      </label>
+      <label htmlFor="name-search">
+        Nome
+        <input
+          id="name-search"
+          type="radio"
+          data-testid="name-search-radio"
+        />
+      </label>
+      <label htmlFor="first-letter">
+        Primeira letra
+        <input
+          id="first-letter"
+          type="radio"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+      <button type="button" data-testid="exec-search-btn">Buscar</button>
+    </div>
+  );
+}
+
+export default function SearchInput() {
+  const [toggleInput, setToggleInput] = useState(false);
+  return (
+    <div>
+      <button type="button" onClick={ () => setToggleInput(!toggleInput) }>
         <img src={ searchIcon } alt="search" data-testid="search-top-btn" />
       </button>
-      { toogleInput && <input type="text" data-testid="search-input" />}
+      { toggleInput && renderSearchBar()}
     </div>
   );
 }
