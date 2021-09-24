d06e138695d14d2a5322ce06f547931ab43815c8
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-24 17:03:45 -0300

    RecipesInProgress.js substituído por ProgressRecipeDrink.js e ProgressRecipeFood.js

---
+++ src/pages/ProgressRecipeFood.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Header from '../components/Header';
+
+export default function RecipesInProgress(meal) {
+  
+  return (
+    <div>
+      <Header title="Receitas em progresso" />
+    </div>
+  );
+}
--- src/pages/RecipesInProgress.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RecipesInProgress() {
-  return (
-    <div>
-      Receitas em progresso
-    </div>
-  );
-}
