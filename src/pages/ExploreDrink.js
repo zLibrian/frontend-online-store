b61096d76fb3c0b85e3aedb739d91bac83c9a97a
Alex <alexcorreas1994@gmail.com>
2021-09-24 16:30:19 -0300

    Requisitos 70-73

--- src/pages/ExploreDrink.js
+++ src/pages/ExploreDrink.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,14 @@ export default function ExploreDrink() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button type="button" data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <button type="button" data-testid="explore-surprise">
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
