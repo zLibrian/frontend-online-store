7bfb8e9da176889d809cd4eb6803002dc7d65bd7
Alex <alexcorreas1994@gmail.com>
2021-09-28 18:28:05 -0300

    Requisito 74 feito

--- src/pages/ExploreDrink.js
+++ src/pages/ExploreDrink.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreDrink() {
+  const [surpriseDrink, setSurpriseDrink] = useState(0);
+  useEffect(() => {
+    async function fetchSurpriseDrink() {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+      const response = await fetch(url).then((res) => res.json());
+      return setSurpriseDrink(response.drinks[0].idDrink);
+    }
+    fetchSurpriseDrink();
+  }, []);
+
+  const idRandom = surpriseDrink;
+
+  function randomDrinks() {
+    if (idRandom > 0) {
+      return (
+        <Link to={ `/bebidas/${idRandom}` }>
+          <button
+            type="button"
+            data-testid="explore-surprise"
+            name="Me Surpreenda"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      );
+    }
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <Header title="Explorar Bebidas" />
@@ -12,9 +41,9 @@ export default function ExploreDrink() {
           Por Ingredientes
         </button>
       </Link>
-      <button type="button" data-testid="explore-surprise">
-        Me Surpreenda!
-      </button>
+      {
+        randomDrinks()
+      }
       <Footer />
     </div>
   );
--- src/pages/ExploreFood.js
+++ src/pages/ExploreFood.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreFood() {
+  const [surpriseMeal, setSurpiseMeal] = useState(0);
+
+  useEffect(() => {
+    async function fetchSurpriseFood() {
+      const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+      const response = await fetch(url).then((res) => res.json());
+      return setSurpiseMeal(response.meals[0].idMeal);
+    }
+    fetchSurpriseFood();
+  }, []);
+
+  const idRandom = surpriseMeal;
+
+  function randomMeals() {
+    if (idRandom > 0) {
+      return (
+        <Link to={ `/comidas/${idRandom}` }>
+          <button
+            type="button"
+            data-testid="explore-surprise"
+            name="Me Surpreenda"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      );
+    }
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <Header title="Explorar Comidas" />
@@ -17,9 +47,9 @@ export default function ExploreFood() {
           Por Local de Origem
         </button>
       </Link>
-      <button type="button" data-testid="explore-surprise">
-        Me Surpreenda!
-      </button>
+      {
+        randomMeals()
+      }
       <Footer />
     </div>
   );
