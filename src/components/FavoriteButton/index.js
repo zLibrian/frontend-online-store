7e56e4c75ca537f50885331d8320cfe2bf120c63
Alex <alexcorreas1994@gmail.com>
2021-10-04 16:40:31 -0300

    Requisito 46

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -3,17 +3,36 @@ import React, { useState } from 'react';
 import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../../images/blackHeartIcon.svg';
 
-export default function FavoriteButton() {
+export default function FavoriteButton({ cardFavorite, type }) {
   const [favorited, setFavorited] = useState(false);
 
   const imageSource = favorited ? blackHeartIcon : whiteHeartIcon;
+
+  function handleClick() {
+    setFavorited(!favorited);
+    if (!localStorage.favoriteRecipes) {
+      localStorage.setItem('favoriteRecipes', JSON.stringify([]));
+    }
+    const localStorageFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const object = {
+      id: cardFavorite[`id${type}`],
+      type,
+      area: cardFavorite[`str${type}`],
+      category: cardFavorite.strCategory,
+      alcoholicOrNot: cardFavorite.strAlcoholic,
+      name: cardFavorite[`str${type}`],
+      image: cardFavorite[`str${type}Thumb`],
+    };
+    const newLocalStorage = [...localStorageFavorite, object];
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newLocalStorage));
+  }
 
   return (
     <div>
       <button
         type="button"
         data-testid="favorite-btn"
-        onClick={ () => setFavorited(!favorited) }
+        onClick={ handleClick }
         src={ imageSource }
       >
         <img src={ imageSource } alt="Botão de favorito" />
